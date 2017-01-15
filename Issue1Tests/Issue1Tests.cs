3c1a27e72b1e3a14513b8408f267577beca2bec4
chensourceforge <chensourceforge@users.noreply.github.com>
2017-01-15 05:57:50 -0500

    Fixed #1, #2 - off-model-route students can now register & build timetable online
    The code to allow off the model route students to register for re-admission and to build timetables online is actually part of the initial commit. I just added the unit tests here.
    
    The solution involves adding front-end controls to let a student indicate their intention of re-admission, and passing the relevant info to the back-end to register the student. Once this student's status looks like a regular student's, the system just treats them like any regular student, thus the student should be able to build their timetable.

---
+++ Issue1Tests/Issue1Tests.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MockCentennial.Models;
+using MockCentennial.Models.TranscriptModel;
+
+namespace Issue1Tests
+{
+    [TestClass]
+    public class CreateStudentTests
+    {
+        private string testVariablesFile = @"../../../../MockCentennial_test_variables.dat";
+
+        private int GetNextStudentNum()
+        {
+            int studentNum = 1;
+            if (File.Exists(testVariablesFile))
+            {
+                using (BinaryReader reader = new BinaryReader(File.OpenRead(testVariablesFile)))
+                {
+                    studentNum = reader.ReadInt32();
+                }
+                studentNum++;
+                using (BinaryWriter writer = new BinaryWriter(File.OpenWrite(testVariablesFile)))
+                {
+                    writer.Write(studentNum);
+                }
+            }
+            else
+            {
+                using (BinaryWriter writer = new BinaryWriter(File.OpenWrite(testVariablesFile)))
+                {
+                    writer.Write(studentNum);
+                }
+            }
+            return studentNum;
+        }
+
+        private int GetLastStudentNum()
+        {
+            if (File.Exists(testVariablesFile))
+            {
+                using (BinaryReader reader = new BinaryReader(File.OpenRead(testVariablesFile)))
+                {
+                    return reader.ReadInt32();
+                }
+            }
+            return 0;
+        }
+
+        [TestMethod]
+        public void NewStudentRegistrationTest()
+        {
+            DateTime registrationDate = new DateTime(2013, 8, 20);
+            int termId = 3, programId = 1, currentSemester = 1;
+
+            int studentNum = GetNextStudentNum();
+            Student student=new Student
+            {
+                StudentNum = $"test{studentNum}",
+                StudentLastName = "LName",
+                StudentFirstMidName = "FName",
+                StudentEmail = "test",
+                StudentHasHolds = false,
+                StudentAcademicStanding = true,
+                StudentCanRegister = true,
+                StudentStartDate = registrationDate
+            };
+
+            CentennialDA dao=new CentennialDA();
+            dao.CreateStudent(ref student);
+
+            int studentId = student.StudentId;
+            Assert.AreNotEqual(0, studentId);
+
+            Console.WriteLine($"StudentId for freshly created student: {studentId}");
+
+            dao.ProgramRegistrationNew(termId, programId, currentSemester, studentId);
+            bool programRegistrationDone = dao.HasValidRegistration(studentId, termId);
+            Assert.IsTrue(programRegistrationDone);
+
+            dao.CreateTranscript(studentId);
+            Transcript transcript = dao.GetStudentTranscript(studentId);
+            Assert.AreEqual($"test{studentNum}", transcript.studentNumber);
+        }
+
+        [TestMethod]
+        public void RegisterStudentForReAdmissionTest()
+        {
+            int termId = 13;
+
+            int studentNum = GetLastStudentNum();
+            Assert.AreNotEqual(0,studentNum);
+
+            CentennialDA dao = new CentennialDA();
+            Student student = dao.FindStudentByStudentNum($"test{studentNum}");
+            Assert.IsNotNull(student);
+
+            int studentId = student.StudentId;
+
+            student.StudentCanRegister = false;
+            dao.UpdateStudent(student);
+            bool registrationEnabled = dao.StudentCanRegister(studentId);
+            Assert.IsFalse(registrationEnabled);
+            
+            bool hasRegistration = dao.HasValidRegistration(studentId, termId);
+            Assert.IsFalse(hasRegistration);
+
+            dao.EnableStudentCanRegister(studentId);
+            registrationEnabled = dao.StudentCanRegister(studentId);
+            Assert.IsTrue(registrationEnabled);
+
+            dao.ProgramRegistrationReAdmit(termId, studentId);
+            hasRegistration = dao.HasValidRegistration(studentId, termId);
+            Assert.IsTrue(hasRegistration);
+        }
+
+    }
+
+    
+}
